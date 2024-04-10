1e222d5b8a1d22def9b801b8097480e0b413ddde
estevaosevero <135609657+estevaosevero@users.noreply.github.com>
2024-04-10 11:46:35 -0300

    Update atividade.py

--- imc/atividade.py
+++ imc/atividade.py
@@ -3,8 +3,9 @@ y = float(input("Digite uma altura:"))
 
 calculadora_de_imc = x/(y*y)
 print( "O imc é:",calculadora_de_imc)
- 
-if calculadora_de_imc < 18.5:
+
+
+if calculadora_de_imc < 18.5 and calculadora_de_imc > 0:
     print("Abaixo do peso")
 elif calculadora_de_imc > 18.5 and calculadora_de_imc < 24.9:
     print("peso normal")
@@ -14,8 +15,10 @@ elif calculadora_de_imc > 30.0 and calculadora_de_imc < 34.9:
     w = calculadora_de_imc * 0.2
     print("Obesidade grau 1, precisa perder:",w)
 elif calculadora_de_imc > 35.0 and calculadora_de_imc < 39.9:
-    p = calculadora_de_imc * 0.2
+    p = calculadora_de_imc * 0.3
     print("Obesidade grau 2 (severa), precisa perder:",p)
+elif calculadora_de_imc >90.0:
+    o = calculadora_de_imc *0.4
+    print(f"Obesidade grau 3 (mórbida),precisa perder:",o)
 else:
-    print("Obesidade grau 3 (mórbida)")
-
+    print("Número não correspondente")
